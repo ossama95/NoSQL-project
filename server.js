199f27d1ba08bd520da682f6f420b527b4739a11
ossama95 <ossama.ougzin@gmail.com>
2018-03-16 11:25:49 +0100

    server

--- server.js
+++ server.js
@@ -15,55 +15,38 @@ app.get('/', function (req, res) {
 })
 
 app.post('/', function (req, res) {
-  let value = req.body.value;
-  let url = `http://localhost:9200/reuters/reuter/_search?q=${value}`
 
-  request(url, function (err, response, body) {
-    if(err){
-      res.render('index', {error: 'Error, please try again'});
-    } else {
-      let result = JSON.parse(body)
-      if(result.hits.total == 0){
-        res.render('index', {result:null,title:null,error: 'Error, please try again'});
-      } else {
-        client.search({
-          index: 'reuters',
-          type: 'reuter',
-          body: {
-            "size": 2557,
-             "min_score": 0.5,
-             "query":
-             {
-               "bool":
-             {
-                 "should": [
-                   {"match":{"fields.text.dateline": "NEW YORK" }}
-                   ],
-                  "must_not": [
-                    {"match": {
-                      "fields.places": "usa"
-                    }}
-                  ]
-             }
-             }
-          }
-        },function (error, response,status) {
-            if (error){
-              console.log("search error: "+error)
-            }
-            else {
-              console.log("--- Response ---");
-              console.log(response);
-              console.log("--- Hits ---");
-              res.render('index', {result:response,error: null});
+    let value = req.body.value;
+    client.search({
+      index: 'reuters',
+      type: 'reuter',
+      body: {
+        "size": 15,
+         "min_score": 0.5,
+         "query":
+         {
+           "bool":
+         {
+             "should": [
+               {"match":{"fields.text.body": value }}
+               ]
+         }
 
-            }
-        });
-
-    //    res.render('index', {result:result,error: null});
+         }
       }
-    }
-  });
+    },function (error, response,status) {
+      if(error){
+        res.render('index', {error: 'Error, please try again'});
+      } else {
+        if(response.hits.total == 0){
+          res.render('index', {result:null,title:null,error: 'Error, please try again'});
+        }
+          res.render('index', {result:response,error: null});
+
+        }
+    });
+
+
 })
 
 app.listen(3000, function () {
